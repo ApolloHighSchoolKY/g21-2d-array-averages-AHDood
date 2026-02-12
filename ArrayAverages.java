9a7af45e56378489f5b055490782ed414c733a16
AHDood <noe.almanza@stu.daviess.kyschools.us>
2026-02-12 05:50:39 +0000

    Commit

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -5,17 +5,43 @@ public class ArrayAverages{
   int[][] ray;
 
   public ArrayAverages(int[][] rayray){
+    
     ray = rayray;
   }
 
   public void rowAverages(){
+    double avg=0;
+
+    for (int row=0; row< ray[0].length; row++)
+    {
+      for(int col=0; col<ray.length; col++)
+      {
+        avg= avg + ray[row][col];
+      }
+      avg = avg/ray[0].length;
     
-  }
+    System.out.println("Row " + row +":" + avg);
+    }
+    }
+  
 
   public void columnAverages(){
     //Column major order
     //   Take each column and go through all of the rows
     //   Top down and Left to Right
+
+    double avg=0;
+
+    for (int col=0; col< ray[0].length; col++)
+    {
+      for(int row=0; row<ray.length; row++)
+      {
+        avg= avg + ray[row][col];
+      }
+      avg = avg/ray[0].length;
+    
+    System.out.println("Column " + col +":" + avg);
+    }
   }
 
   public void arrayAverage(){
--- Gee21.java
+++ Gee21.java
@@ -2,7 +2,7 @@ public class Gee21{
   public static void main(String[] args){
 
     int[][] ray = new int[7][7];
-
+    
     //Populate the array with sequential numbers
     /*
       1  2  3  4  5  6  7
@@ -10,7 +10,15 @@ public class Gee21{
       15 16 17 18 19 20 21
       22 23 24 25...
       */
-
+     int num=1;
+for (int x=0;x<ray.length;x++)
+{
+for (int y=0;y<ray[x].length;y++)
+{
+  ray[x][y]=num;
+  num++;
+}
+}
     ArrayAverages twoDee = new ArrayAverages(ray);
 
     System.out.println(twoDee);
